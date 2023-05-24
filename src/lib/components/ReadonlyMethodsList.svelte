cffe2c56185644487ff0d1bfd010da346e12d41b
ByteAtATime <byteatatime@proton.me>
2023-05-24 21:37:06 +0000

    import missing contract

--- src/lib/components/ReadonlyMethodsList.svelte
+++ src/lib/components/ReadonlyMethodsList.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { AbiItem } from "web3-utils";
   import ReadonlyMethod from "./ReadonlyMethod.svelte";
+  import type { Contract } from "$lib/contract";
 
     export let contract: Contract;
     export let methods: AbiItem[];
